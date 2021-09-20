6e0943f6439cb32968ecec2d584942719a4b6120
Swetaleena Guha <swetaleenaguha@gmail.com>
2021-09-20 10:57:48 +0530

    show method added

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -5,8 +5,10 @@ public class BootClass {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello! from Eclipse!");
-		
-
+		show(); 
+		}
+	public static void show() {
+		System.out.println("Show method here!");
 	}
 
 }
